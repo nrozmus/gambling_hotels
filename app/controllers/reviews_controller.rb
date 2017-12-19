1f7d75f8dcdc60b44d0685f557214ae487dcea02
NickisKnowledge <lifechangingstuff@gmail.com>
2017-12-18 22:00:39 -0600

    WIP;Created strong params and added before_action of 'require_login' so non-logged in users can't access Review routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  before_action :require_login
+
+  private
+    def review_params
+      params.require(:review).permit(
+        :length_of_stay,
+        :comment,
+        :room_type_id,
+        :user_id
+      )
+    end
 end
