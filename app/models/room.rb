0d2c970f3fd68702c2c74b50a5e7350dd10ec081
NickisKnowledge <lifechangingstuff@gmail.com>
2018-01-03 17:48:05 -0600

    WIP; Removed unnecessary method

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,8 +3,4 @@ class Room < ActiveRecord::Base
   belongs_to :hotel
   has_many :reservations
   has_many :users, through: :reservations
-
-  def self.hotel_rooms(id)
-    where('hotel_id = ?', id)
-  end
 end
